3e4bdf12db8eed605f6d59ae9572f79c7c5571e3
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-09-25 12:02:38 +0600

    RMX1901: Add keymaster 4.0
    Fixes:
    W libc    : Unable to set property "ctl.interface_start" to "android.hardware.keymaster@4.0::IKeymasterDevice/default": error code: 0x20
    I hwservicemanager: Tried to start android.hardware.keymaster@4.0::IKeymasterDevice/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- device.mk
+++ device.mk
@@ -195,7 +195,9 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
